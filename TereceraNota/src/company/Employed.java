3a2914add6b889afc094fd91dfc63c454862828b
XxJavier-SernaxX <Jamaseos77@gmail.com>
2025-09-03 20:30:50 -0500

    correcion de ejercicio de encapsulamiento

--- TereceraNota/src/company/Employed.java
+++ TereceraNota/src/company/Employed.java
@@ -11,8 +11,41 @@ public class Employed {
     public void setSalary(double salary) {
         if (salary>0) {
              this.salary = salary;
+        }else 
+
+       System.out.println("el salario debe de ser positivo");
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getRole() {
+        return role;
+    }
+
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+
+    public void increaseSalary(double amount){
+        if (amount>0) {
+            salary+=amount;
+            System.out.println("su nuevo salario es de "+ salary);
+            
+        }else{
+            salary=salary;
         }
-       
+
     }
 
 
--- TereceraNota/src/company/Main.java
+++ TereceraNota/src/company/Main.java
@@ -2,8 +2,18 @@ package company;
 
 public class Main {
 
-    public static void Main(String[]args)
-        Employed employed1 = new Employed();
-        
+    public static void main(String[] args){
+         Employed employed1 = new Employed();
+        employed1.name = "Javier";
+        employed1.role = "Programer";
+        employed1.setSalary(1000); 
+        employed1.increaseSalary(-500);
+       
+    
+        System.out.println(employed1.getSalary()); 
+        System.out.println(employed1.getName());
+        System.out.println(employed1.getRole());
+    }
+       
     
 }
